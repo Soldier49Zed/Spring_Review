c86793df1591e26530fd981517e1ef0c2df3380c
houxiang <15291122393@163.com>
2020-11-18 22:09:43 +0800

    装配Bean的其他方式

---
+++ src/main/java/com/houx/annotation/service/RoleDataSourceService.java
@@ -0,0 +1,12 @@
+package com.houx.annotation.service;
+
+import com.houx.annotation.pojo.Role;
+
+/**
+ * @Author: HouX
+ * @Date: 2020/11/18
+ * @Description:
+ */
+public interface RoleDataSourceService {
+	public Role getRole(Long id);
+}
